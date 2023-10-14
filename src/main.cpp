d85e4559450adc7865cb3bd3f976f0a20f59b6e8
AlexKCA <alexkca@hoth.hoth>
2023-10-14 10:46:06 -0700

    ncurses working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <ncurses.h>
 
 int main()
-{
-  return 0;
+{	
+	initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
+
+	return 0;
 }
