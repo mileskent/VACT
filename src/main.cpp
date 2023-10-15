673f70708d2ab4773c35d3bf6984b00b18dcb9cb
AlexKCA <alexkca@hoth.hoth>
2023-10-15 13:09:05 -0700

    Da windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,25 @@
 #include <ncurses.h>
 
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
-
-int main(int argc, char *argv[])
-{	WINDOW *my_win;
-	int startx, starty, width, height;
-	int ch;
-
+int main( void )
+{
+	const double TWPER = 0.7;
 	initscr();					// Start curses mode
 	cbreak();					// Line Buffering Off
 	keypad(stdscr, TRUE);		// Functions keys, etc
+	refresh();					// Uhhhhhh
 
-	height = LINES;
-	width = COLS;
-	starty = 0;
-	startx = 0;
-	printw("Press F1 to exit");
-	refresh();
-	my_win = create_newwin(height, width, starty, startx);
-	getch();
-	endwin();			/* End curses mode		  */
+	// Create text window
+	WINDOW * text_window = newwin(LINES, COLS * TWPER, 0, 0); 	// Da window
+	box(text_window, 0, 0);		// Box idk
+	wrefresh(text_window); 		// Show the box
+	
+	// Create tooltip window
+	WINDOW * tt_window = newwin(LINES, COLS * (1 - TWPER), 0, COLS * TWPER);
+	box(tt_window, 0, 0);
+	wrefresh(tt_window);
+
+	// End exec
+	getch();					// Wait for key to exit
+	endwin();					// End curses mode
 	return 0;
-}
-
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{	WINDOW *local_win;
-
-	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
-
-	return local_win;
 }
