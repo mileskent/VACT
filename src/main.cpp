68b3e9776d1aa657ffff9b5270cbb20234c7ad0e
AlexKCA <alexkca@hoth.hoth>
2023-10-15 12:07:04 -0700

    Let there be a window!

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
 #include <ncurses.h>
 
-int main()
-{	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+void destroy_win(WINDOW *local_win);
 
+int main(int argc, char *argv[])
+{	WINDOW *my_win;
+	int startx, starty, width, height;
+	int ch;
+
+	initscr();					// Start curses mode
+	cbreak();					// Line Buffering Off
+	keypad(stdscr, TRUE);		// Functions keys, etc
+
+	height = LINES;
+	width = COLS;
+	starty = 0;
+	startx = 0;
+	printw("Press F1 to exit");
+	refresh();
+	my_win = create_newwin(height, width, starty, startx);
+	getch();
+	endwin();			/* End curses mode		  */
 	return 0;
+}
+
+WINDOW *create_newwin(int height, int width, int starty, int startx)
+{	WINDOW *local_win;
+
+	local_win = newwin(height, width, starty, startx);
+	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
+					 * for the vertical and horizontal
+					 * lines			*/
+	wrefresh(local_win);		/* Show that box 		*/
+
+	return local_win;
 }
