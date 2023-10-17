80ac0b2585ea95d452e6426392f740b3d0143280
AlexKCA <alexkca@endor.endor>
2023-10-16 22:33:11 -0700

    Seems like massive error with strings so I think I will switch to one big vector<> because larger texts can be outside of the scope of this project

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,28 @@ int main (void)
 	// Create text window
 	// TODO: Scrollable text window
 	WINDOW * text_window = newwin(LINES, (int)(COLS * TWPER), 0, 0); 	// Da window
-	char text_title[] = "Text";
-	mvwprintw (text_window, 0, (int)(COLS * TWPER / 2) - strlen(text_title) / 2, text_title); // Title
 	int wrap = (int)(COLS * TWPER - 3);
 	string contents = slurp ("book.txt", wrap); 
 		
 	// TODO: Print from line A to line B
-	mvwprintw (text_window, 1, 1, contents.c_str());
-	box (text_window, 0, 0);		// Box idk
+	int A, B, start, end;
+	A = 0;
+	B = 4;
+	start = 0;
+	string line;
+	for (int i = A; i <= B; i++)
+	{
+		if (start < contents.length())
+		{
+			end = contents.find("\n", start);
+			line = contents.substr(start, end);
+			start = end + 1;
+			mvwprintw (text_window, 1 + i - A, 1, line.c_str());
+		}
+	}
+	// box (text_window, 0, 0);		// Draw a box border for the window
+	char text_title[] = "Text";
+	mvwprintw (text_window, 0, (int)(COLS * TWPER / 2) - strlen(text_title) / 2, text_title); // Title
 	wrefresh (text_window);
 	refresh ();
 	
