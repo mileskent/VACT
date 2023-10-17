accccbaa7d06547795db49a04533be9133f92ed3
AlexKCA <alexkca@endor.endor>
2023-10-17 09:38:38 -0700

    It's all coming together :D (Scroll up and down, conditional highlighting)

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,34 @@
 
 using namespace std;
 
+WINDOW * text_box;
+WINDOW * text_window;
+WINDOW * tt_window;
+int first_word = 0; const int WORD_CAP = 200;
+const double TWPER = 0.7;
+vector<string> blocks;
 
-int main (void)
+int print_words (void)
 {
-	// init word stuff
-	vector<string> blocks = slurp ("book.txt");
-	const int WORD_CAP = 200;
-
-	// init ncurses stuff
-	const double TWPER = 0.7;
-	initscr();					// Start curses mode
-	cbreak();					// Line Buffering Off
-	keypad(stdscr, TRUE);		// Functions keys, etc
-	refresh();					// Uhhhhhh
-
-	// Create text window
-	// TODO: Scrollable text window
-	
-	WINDOW * text_box = newwin(LINES, (int)(COLS * TWPER), 0, 0); 	
-	WINDOW * text_window = newwin(LINES, (int)(COLS * TWPER) - 2, 0, 1); 	// Da window
-	// int wrap = (int)(COLS * TWPER - 3); // we can do wrap later
-
 	// We have to print word by word
-	int A = 0;
 	wmove (text_window, 1, 0); wrefresh (text_window);
-	for (int i = A; i < A + WORD_CAP; i++)
+	for (int i = first_word; i < first_word + WORD_CAP; i++)
 	{
 		if (i > blocks.size() - 1) break;
-		string block = blocks.at(i) + " ";
-		wprintw (text_window, block.c_str());
+
+		if (i % 2 == 0)
+		{
+			wattron(text_window, A_BOLD | A_BLINK);
+			wprintw (text_window, blocks.at(i).c_str());
+			wattroff(text_window, A_BOLD | A_BLINK);	
+		}
+		else 
+		{
+			wprintw (text_window, blocks.at(i).c_str());
+		}
+
+
+		wprintw (text_window, " ");
 	}
 
 	box (text_box, 0, 0);		// Draw a box border for the window
@@ -42,16 +41,67 @@ int main (void)
 	wrefresh (text_box);
 	wrefresh (text_window);
 	refresh ();
+
+	return 0;
+}
+
+
+
+int main (void)
+{
+
+
+// init word stuff
+	blocks = slurp ("book.txt");
+
+
+// init ncurses stuff
+	initscr();					// Start curses mode
+	cbreak();					// Line Buffering Off
+	keypad(stdscr, TRUE);		// Functions keys, etc
+	start_color();				// colorzzzzz
+	refresh();					// The documentation has this here so Im copying it
+
+
+// init screen
+	// Create text window
+	text_box = newwin(LINES, (int)(COLS * TWPER), 0, 0); 	
+	text_window = newwin(LINES, (int)(COLS * TWPER) - 2, 0, 1);	
+	// int wrap = (int)(COLS * TWPER - 3); // we can do wrap later
+
+	print_words ();
 	
 	// Create tooltip window
-	WINDOW * tt_window = newwin (LINES, (int)(COLS * (1 - TWPER)), 0, (int)(COLS * TWPER));
+	tt_window = newwin (LINES, (int)(COLS * (1 - TWPER)), 0, (int)(COLS * TWPER));
 	box (tt_window, 0, 0);
 	char tt_title[] = "Tooltip";
 	mvwprintw (tt_window, 0, (int)(COLS * (1 - TWPER) / 2) - strlen(tt_title) / 2, tt_title);
 	wrefresh (tt_window);
 
-	// End exec
-	getch();					// Wait for key to exit
+
+// input
+	int inpch;
+	while((inpch = wgetch(text_window)) != 'q')
+	{ 
+		switch(inpch)
+		{ 
+			case 'j':
+				first_word+=50;
+				if (first_word > blocks.size() - WORD_CAP) first_word = blocks.size() - WORD_CAP;
+				print_words();
+				break;
+			case 'k':
+				first_word-=50;
+				if (first_word < 0) first_word = 0;
+				print_words();
+				break;
+		}
+		wmove (text_window, LINES, 1); wrefresh (text_window);
+	}
+
+
+// End exec
+	// getch();					// Wait for key to exit
 	endwin();					// End curses mode
 	return 0;
 }
